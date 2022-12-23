7d5a43fd52b0b4916d1fcaba98824c8518b2ff53
Mohamed Elkordi <111140435+Mohamed-Elkordi@users.noreply.github.com>
2022-12-23 18:30:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showList() {
+    var list = document.getElementById("navigation");
+    if(list.style.display=='block'){
+        list.style.display='none';
+    }
+    else {
+        list.style.display='block';
+    }
+}
